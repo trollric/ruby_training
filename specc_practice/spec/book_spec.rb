6502a5221a364f885cebcfbce84fbd65bcfa11da
Marcus Hansson <90m.hansson@gmail.com>
2017-09-15 16:05:57 +0000

    Written som very basic testing code

--- specc_practice/spec/book_spec.rb
+++ specc_practice/spec/book_spec.rb
@@ -1,12 +1,37 @@
-require "./spec_helper"
+require "spec_helper"
 
 describe Book do
+
   before :each do
     @book = Book.new "Title", "Author", :category
   end
+
   describe "#new" do
-    it "takes three parameters and returns a Book object" do
+    it "returns a new book object" do
       @book.should be_an_instance_of Book
     end
+
+    it "Throws an ArgumentError when given fewer than 3 parameters" do
+      lambda{Book.new "title", "author"}.should raise_exception ArgumentError
+    end
   end
+
+  describe '#title' do
+    it "returns the correct title" do
+      @book.title.should eql "Title"
+    end
+  end
+
+  describe '#category' do
+    it "returns the correct category" do
+      @book.category.should eql :category
+    end
+  end
+
+  describe '#author' do
+    it "returns the correct author" do
+      @book.author.should eql "Author"
+    end
+  end
+
 end
